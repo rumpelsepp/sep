efb749a101a9343635e04af8a8209483457d7fba
Stefan Tatschner <stefan@rumpelsepp.org>
2019-07-24 16:46:34 +0200

    Add helpers for generating keys

--- util.go
+++ util.go
@@ -1,10 +1,22 @@
 package sep
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
 	"io"
+	"math/big"
 	"sync"
+	"time"
 
+	"git.sr.ht/~rumpelsepp/ni"
 	"git.sr.ht/~rumpelsepp/rlog"
+	"golang.org/x/crypto/sha3"
 )
 
 // TODO: error
@@ -46,4 +58,81 @@ func BidirectCopy(left io.ReadWriteCloser, right io.ReadWriteCloser) (int, int, 
 	wg.Wait()
 
 	return n1, n2, err1, err2
+}
+
+func GenKeypairPEM() ([]byte, []byte, error) {
+	priv, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	if err != nil {
+		return nil, nil, fmt.Errorf("generate private key: %s", priv)
+	}
+
+	pubDer, err := x509.MarshalPKIXPublicKey(&priv.PublicKey)
+	if err != nil {
+		return nil, nil, fmt.Errorf("marshall private key: %s", priv)
+	}
+
+	notBefore := time.Now()
+	notAfter := time.Date(2049, 12, 31, 23, 59, 59, 0, time.UTC)
+	digest := sha3.Sum256(pubDer)
+
+	niURL, err := ni.DigestToNI(digest[:], "sha3-256", DefaultResolveDomain)
+	if err != nil {
+		return nil, nil, fmt.Errorf("parsing digest: %s", err)
+	}
+
+	fingerprint, err := FingerprintFromRawNI(niURL)
+	if err != nil {
+		return nil, nil, fmt.Errorf("parsing digest: %s", err)
+	}
+
+	commonName := fingerprint.FQDN()
+
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return nil, nil, fmt.Errorf("generate serial number: %s", err)
+	}
+
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			CommonName: commonName,
+		},
+		NotBefore: notBefore,
+		NotAfter:  notAfter,
+
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
+		BasicConstraintsValid: true,
+	}
+
+	derCert, err := x509.CreateCertificate(rand.Reader, &template, &template, priv.Public(), priv)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: derCert})
+
+	b, err := x509.MarshalECPrivateKey(priv)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "EC PRIVATE KEY", Bytes: b})
+
+	return certPEM, keyPEM, nil
+}
+
+func GenKeypair() (tls.Certificate, error) {
+	certPEM, keyPEM, err := GenKeypairPEM()
+	if err != nil {
+		return tls.Certificate{}, err
+	}
+
+	keypair, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		return tls.Certificate{}, err
+	}
+
+	return keypair, nil
 }
