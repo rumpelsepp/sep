5b0abdf5cdd18f0ea95534f340b33e6ac140d71b
Jarisch, Ferdinand <ferdinand.jarisch@aisec.fraunhofer.de>
2019-08-06 21:39:18 +0200

    Implement MNDListener for UDP Broadcast Discovery

--- mnd.go
+++ mnd.go
@@ -1,6 +1,7 @@
 package sep
 
 import (
+	"bufio"
 	"bytes"
 	"crypto"
 	"crypto/ecdsa"
@@ -11,6 +12,7 @@ import (
 	"errors"
 	"fmt"
 	"net"
+	"strconv"
 	"time"
 
 	"git.sr.ht/~rumpelsepp/ni"
@@ -107,6 +109,201 @@ func mndListenForResponse(targetFp *Fingerprint, timeoutDuration time.Duration) 
 			return response, nil
 		}
 	}
+}
+
+// MNDListener responds to MNDDiscoverRequests.
+type MNDListener struct {
+	privateKey crypto.PrivateKey
+	payload    *DirectoryRecordSet
+
+	running bool
+	close   chan bool
+	timeout *time.Timer
+
+	ownFp      *Fingerprint
+	trustedFPs []*Fingerprint
+}
+
+// NewMNDListener serves as constructor of the MNDListener type which responds
+// to MNDDiscoverRequests.
+func NewMNDListener(ownFp *Fingerprint, privateKey crypto.PrivateKey, trustedFPs []*Fingerprint) MNDListener {
+	timer := time.NewTimer(5 * time.Second)
+	timer.Stop()
+	return MNDListener{
+		ownFp:      ownFp,
+		privateKey: privateKey,
+		payload:    &DirectoryRecordSet{},
+		close:      make(chan bool, 1),
+		running:    false,
+		timeout:    timer,
+		trustedFPs: trustedFPs,
+	}
+}
+
+// Close stops the MNDListener.
+func (m *MNDListener) Close() {
+	m.close <- true
+}
+
+// ServeRecordSet updates the RecordSet that is served as a reply for valid Discovery
+// requests.
+// If not already running, it moreover starts a Listener on the given address and
+// DefaultMNDDiscoveryPort which validates incoming MNDDiscoverRequests, signs the
+// RecordSet and responds to the requester.
+//
+// The Listener terminates either when the Close() method is called or when the
+// RecordSet expires (defined by the TTL).
+func (m *MNDListener) ServeRecordSet(recordSet *DirectoryRecordSet, listenAddr string) error {
+	logger.Debug("Updating RecordSet of MND listener")
+	m.payload = recordSet
+	m.payload.Sign(m.privateKey)
+
+	// This is an length check of the resulting record, arbitrarily set to 500
+	var b bytes.Buffer
+	writer := bufio.NewWriter(&b)
+	encoder := cbor.NewEncoder(writer, cbor.EncOptions{Canonical: true})
+	if err := encoder.Encode(m.payload); err != nil {
+		return err
+	}
+	if writer.Buffered() > 500 || writer.Buffered() == 0 {
+		logger.Warning(writer.Buffered())
+		return fmt.Errorf("RecordSet too large")
+	}
+
+	m.timeout.Reset(time.Duration(m.payload.TTL) * time.Second)
+
+	if !m.running {
+		logger.Debug("Starting MND listener")
+		m.running = true
+		if err := m.listen(listenAddr); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Listen starts a listener on the given address and DefaultMNDDiscoveryPort
+// which validates incoming MNDDiscoverRequests, signs the RecordSet given by
+// the ServeRecordSet() method and responds to the requester.
+//
+// The Listener terminates either when the Close() method is called or when the
+// RecordSet given by the ServeRecordSet() method expires.
+func (m *MNDListener) listen(listenAddr string) error {
+	requestPayload := &DirectoryRecordSet{}
+
+	addr, err := net.ResolveUDPAddr("udp", net.JoinHostPort(listenAddr, DefaultMNDDiscoverPort))
+	if err != nil {
+		return err
+	}
+
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		for {
+			select {
+			case <-m.timeout.C:
+				logger.Info("MNDListener timed out")
+				m.running = false
+				conn.Close()
+				return
+			case <-m.close:
+				logger.Info("MNDListener received close request")
+				m.running = false
+				conn.Close()
+				return
+			}
+		}
+	}()
+
+	go func() {
+		// It was checked before that the encoded RecordSet is smaller than 500
+		buff := make([]byte, 500)
+
+		for m.running {
+			n, senderAddress, err := conn.ReadFromUDP(buff)
+			if err != nil {
+				logger.Debugf("error while decoding: %s", err)
+				continue
+			}
+			logger.Debugf("Received packet from %s", senderAddress.String())
+
+			// Decode packet
+			decoder := cbor.NewDecoder(bytes.NewReader(buff[:n]))
+			if err := decoder.Decode(requestPayload); err != nil {
+				logger.Debugf("could not parse payload: %s", err)
+				continue
+			}
+
+			// Check target
+			if !bytes.Equal(requestPayload.Blob, m.ownFp.Bytes()) {
+				logger.Debugf("got request for '%s'; am '%s'", requestPayload.Blob, m.ownFp.Bytes())
+				continue
+			}
+			logger.Debugf("Request is addressed to me")
+
+			// Validate packet signature
+			reqFp, err := FingerprintFromPublicKey(requestPayload.PubKey, DefaultFingerprintSuite, "")
+			if err != nil {
+				logger.Debugf("got response with non-parsable public key: %s", err)
+				continue
+			}
+			signatureOk, err := requestPayload.CheckSignature(reqFp)
+			if err != nil {
+				logger.Debugf("signature check failed: %s", err)
+				continue
+			}
+			if !signatureOk {
+				logger.Infof("signature check failed: %s", err)
+				continue
+			}
+			logger.Debugf("Request has valid signature")
+
+			// Check whether sender is trusted
+			trusted := false
+			for _, fp := range m.trustedFPs {
+				if FingerprintIsEqual(reqFp, fp) {
+					trusted = true
+					break
+				}
+			}
+			if !trusted {
+				logger.Debug("Request comes from untrusted node")
+				continue
+			}
+			logger.Debug("Request comes from trusted node")
+
+			// Prepare response address
+			if senderAddress.Port, err = strconv.Atoi(DefaultMNDResponsePort); err != nil {
+				logger.Warningf("could not parse DefaultMNDResponsePort")
+				continue
+			}
+
+			// Send response
+			respConn, err := net.DialUDP("udp", nil, senderAddress)
+			if err != nil {
+				logger.Warning(err)
+				continue
+			}
+			defer respConn.Close()
+
+			// Hardcode TTL to 5 for low lifetimes on local networks
+			m.payload.TTL = 5
+			m.payload.Sign(m.privateKey)
+
+			encoder := cbor.NewEncoder(respConn, cbor.EncOptions{Canonical: true})
+			if err := encoder.Encode(m.payload); err != nil {
+				logger.Warning(err)
+				continue
+			}
+			logger.Debugf("Sent response to %s", senderAddress.String())
+		}
+	}()
+
+	return nil
 }
 
 // From here it's untouched legacy stuff
