6d069514a301235d74477f8aa3e174e1540b311e
Stefan Tatschner <stefan@rumpelsepp.org>
2019-08-19 09:38:55 +0200

    Remove unused stuff from X.509 certificate.

--- util.go
+++ util.go
@@ -8,7 +8,6 @@ import (
 	"crypto/rand"
 	"crypto/tls"
 	"crypto/x509"
-	"crypto/x509/pkix"
 	"encoding/binary"
 	"encoding/pem"
 	"errors"
@@ -21,11 +20,8 @@ import (
 	"runtime"
 	"strings"
 	"sync"
-	"time"
 
-	"git.sr.ht/~rumpelsepp/ni"
 	"git.sr.ht/~rumpelsepp/rlog"
-	"golang.org/x/crypto/sha3"
 	"golang.org/x/xerrors"
 )
 
@@ -46,7 +42,6 @@ func BidirectCopy(left io.ReadWriteCloser, right io.ReadWriteCloser) (int, int, 
 
 	go func() {
 		if n, err := io.Copy(right, left); err != nil {
-			rlog.Debugln(err)
 			err1 = err
 		} else {
 			n1 = int(n)
@@ -58,7 +53,6 @@ func BidirectCopy(left io.ReadWriteCloser, right io.ReadWriteCloser) (int, int, 
 
 	go func() {
 		if n, err := io.Copy(left, right); err != nil {
-			rlog.Debugln(err)
 			err2 = err
 		} else {
 			n2 = int(n)
@@ -91,45 +85,13 @@ func GenKeypairPEM() ([]byte, []byte, error) {
 		return nil, nil, fmt.Errorf("generate private key: %s", priv)
 	}
 
-	pubDer, err := x509.MarshalPKIXPublicKey(&priv.PublicKey)
-	if err != nil {
-		return nil, nil, fmt.Errorf("marshall private key: %s", priv)
-	}
-
-	notBefore := time.Now()
-	notAfter := time.Date(2049, 12, 31, 23, 59, 59, 0, time.UTC)
-	digest := sha3.Sum256(pubDer)
-
-	niURL, err := ni.DigestToNI(digest[:], "sha3-256", DefaultResolveDomain)
-	if err != nil {
-		return nil, nil, fmt.Errorf("parsing digest: %s", err)
-	}
-
-	fingerprint, err := FingerprintFromRawNI(niURL)
-	if err != nil {
-		return nil, nil, fmt.Errorf("parsing digest: %s", err)
-	}
-
-	commonName := fingerprint.FQDN()
-
 	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
 	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
 	if err != nil {
 		return nil, nil, fmt.Errorf("generate serial number: %s", err)
 	}
 
-	template := x509.Certificate{
-		SerialNumber: serialNumber,
-		Subject: pkix.Name{
-			CommonName: commonName,
-		},
-		NotBefore: notBefore,
-		NotAfter:  notAfter,
-
-		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
-		BasicConstraintsValid: true,
-	}
+	template := x509.Certificate{SerialNumber: serialNumber}
 
 	derCert, err := x509.CreateCertificate(rand.Reader, &template, &template, priv.Public(), priv)
 	if err != nil {
