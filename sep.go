003de06419ae58fd4cbfb5b652709c280af94802
Stefan Tatschner <stefan@rumpelsepp.org>
2019-09-02 14:29:42 +0200

    Fix typo Sep vs. SEP.

--- sep.go
+++ sep.go
@@ -180,13 +180,13 @@ var tlsCipherSuiteNames = map[uint16]string{
 	0xcca9: "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305",
 }
 
-type SepVerifier func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error
+type SEPVerifier func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error
 
 func VerifierAllowAll(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
 	return nil
 }
 
-func MakeDefaultVerifier(allowed []*Fingerprint, database TrustDatabase) SepVerifier {
+func MakeDefaultVerifier(allowed []*Fingerprint, database TrustDatabase) SEPVerifier {
 	return func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
 		for _, cert := range rawCerts {
 			remoteFP, err := FingerprintFromCertificate(cert, DefaultFingerprintSuite, "")
